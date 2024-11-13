91d8000c60d4f67964c9f638bf05d6ec8e2eeed6
Lucia-Susanna <lucia_susanna@hotmail.it>
2024-11-13 15:57:13 +0100

    feat: calc full price

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
+let kmViaggiatore = parseInt(prompt('Inserisci numero di km'));
+let etaViaggiatore = parseInt(prompt('Inserisci età viaggiatore'));
 
+const prezzoKm = 0.21
+
+const prezzoIntero = (kmViaggiatore * prezzoKm).toFixed(2);
+console.log(prezzoIntero);
